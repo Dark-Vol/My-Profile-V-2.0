f489358da5c794c266b3a567a8f121c45f711494
Dark-Vol <overlord28985@gmail.com>
2026-01-19 00:28:33 +0200

    update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -28,5 +28,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'certificates',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
