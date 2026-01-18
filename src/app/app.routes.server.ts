dbe4106e02fd3effd24e999c259237494a81b242
Dark-Vol <overlord28985@gmail.com>
2026-01-19 00:25:16 +0200

    update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,31 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'projects',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'projects/technology/*',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'projects/*',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'technologies',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'technologies/*',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'certificates',
     renderMode: RenderMode.Prerender
   }
 ];
