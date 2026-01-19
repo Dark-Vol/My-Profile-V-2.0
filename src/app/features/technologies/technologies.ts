8377c37120ea3ebb19edd8d88fbda00f110c7480
Dark-Vol <overlord28985@gmail.com>
2026-01-19 16:32:44 +0200

    add content and Router

--- src/app/features/technologies/technologies.ts
+++ src/app/features/technologies/technologies.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-technologies',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './technologies.html',
   styleUrl: './technologies.scss',
 })
